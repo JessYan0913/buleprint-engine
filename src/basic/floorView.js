52b198b9bb91cb20b15595776a4bfc0241019468
严恒 <yanheng@tinkers.com.cn>
2021-04-08 10:42:00 +0800

    修改比例计算公式

--- src/basic/floorView.js
+++ src/basic/floorView.js
@@ -3,17 +3,18 @@ import { svg } from "d3-fetch";
 import { select } from "d3-selection";
 import "../assets/structure_1.svg";
 import "../assets/beam_1.svg";
-import '../assets/runway_1.svg'
+import "../assets/runway_1.svg";
 
 class PartImage {
   constructor(props) {
-    const { image, relWidth, relHeight, transfer, scale } = props;
+    const { name, image, realWidth, realHeight, transfer, scale } = props;
+    this.name = name;
     this.image = `/img/${image}`;
-    const { x, y} = transfer;
+    const { x, y } = transfer;
     this.transferX = x * scale;
     this.transferY = y * scale;
-    this.relWidth = relWidth;
-    this.relHeight = relHeight;
+    this.realWidth = realWidth;
+    this.realHeight = realHeight;
     this.scale = scale;
   }
 
@@ -25,18 +26,27 @@ class PartImage {
         "viewBox",
         `0 0 ${partGroup.attr("width")} ${partGroup.attr("height")}`
       )
-      .attr("width", this.scale * this.relWidth)
-      .attr("height", this.scale * this.relHeight);
+      .attr("width", this.scale * this.realWidth)
+      .attr("height", this.scale * this.realHeight);
     return partGroup;
   }
 }
 
 export default class ThreeView {
   constructor(props) {
-    const { container, width, height, parts } = props;
+    const {
+      container,
+      width,
+      height,
+      realWidth,
+      realHeight,
+      parts = [],
+    } = props;
     this.container = container;
     this.width = width;
     this.height = height;
+    this.realWidth = realWidth;
+    this.realHeight = realHeight;
     this.parts = parts || [];
 
     this.svg = select(container)
@@ -45,9 +55,24 @@ export default class ThreeView {
       .attr("width", width)
       .attr("height", height);
 
-    const maxRelWidth = max(parts, (item) => item.relWidth);
-    const maxRelHeight = max(parts, (item) => item.relHeight);
-    this.scale = min([width / maxRelWidth, height / maxRelHeight]);
+    const mergeArray = (sourceArr, targetArr) =>
+      sourceArr.reduce((pre, cur) => {
+        pre.push(cur);
+        return pre;
+      }, targetArr);
+
+    const widths = mergeArray(
+      parts.map((item) => item.width),
+      [realWidth]
+    );
+    const heights = mergeArray(
+      parts.map((item) => item.height),
+      [realHeight]
+    );
+
+    const maxrealWidth = max(widths);
+    const maxrealHeight = max(heights);
+    this.scale = min([width / maxrealWidth, height / maxrealHeight]);
 
     this.partImages = this.parts.map(
       (item) => new PartImage({ ...item, scale: this.scale })
@@ -59,7 +84,7 @@ export default class ThreeView {
       const partGroup = await item.render();
       this.svg
         .append("g")
-        .attr('transform', `translate(${item.transferX} ${item.transferY})`)
+        .attr("transform", `translate(${item.transferX} ${item.transferY})`)
         .append(() => partGroup.node());
     });
   }
