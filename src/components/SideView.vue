9237fe43a3ba2c3a6407e4bdf10a5acca1075b3d
严恒 <yanheng@tinkers.com.cn>
2021-04-27 16:53:46 +0800

    调整组件素材

--- src/components/SideView.vue
+++ src/components/SideView.vue
@@ -39,7 +39,7 @@ export default {
       type: Object,
       default() {
         return {
-          image: 'beam_2.svg',
+          image: 'beam_side.svg',
           realLength: 6000,
           realWidth: 400,
           realHeight: 400,
@@ -50,7 +50,7 @@ export default {
       type: Object,
       default() {
         return {
-          image: 'runway_2.svg',
+          image: 'runway_side.svg',
           realLength: 6000,
           realWidth: 200,
           realHeight: 400,
