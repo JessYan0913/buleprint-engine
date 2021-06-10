2b8c716c61c5460bc31663d379b9c0fcd0659895
严恒 <yanheng@tinkers.com.cn>
2021-06-10 11:38:03 +0800

    删除vue相关内容

--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-const files = require.context("./", false, /\.svg$/);
-const modules = {};
-
-files.keys().forEach((key) => {
-    modules[key.replace(/(\.\/|\.svg)/g, '')] = files(key).default
-});
-
-export default modules;
